7c2fed18308fb0c724ef858cfbdce6de7f8ab8e0
Seth Seylerius <seylerius@seriouslyseylerius.com>
2019-12-11 00:31:37 +0000

    Update dependencies

--- project.clj
+++ project.clj
@@ -4,19 +4,19 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.9.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [ring-server "0.5.0"]
                  [reagent "0.8.1"]
-                 [reagent-utils "0.3.1"]
-                 [ring "1.6.3"]
-                 [ring/ring-defaults "0.3.1"]
+                 [reagent-utils "0.3.3"]
+                 [ring "1.8.0"]
+                 [ring/ring-defaults "0.3.2"]
                  [compojure "1.6.1"]
                  [hiccup "1.0.5"]
-                 [yogthos/config "1.1.1"]
-                 [org.clojure/clojurescript "1.10.339"
+                 [yogthos/config "1.1.7"]
+                 [org.clojure/clojurescript "1.10.597"
                   :scope "provided"]
                  [secretary "1.2.3"]
-                 [venantius/accountant "0.2.4"
+                 [venantius/accountant "0.2.5"
                   :exclusions [org.clojure/tools.reader]]]
 
   :plugins [[lein-environ "1.1.0"]
@@ -82,15 +82,15 @@
 
 
   :profiles {:dev {:repl-options {:init-ns jumpdock.repl}
-                   :dependencies [[cider/piggieback "0.3.8"]
-                                  [binaryage/devtools "0.9.10"]
-                                  [ring/ring-mock "0.3.2"]
-                                  [ring/ring-devel "1.6.3"]
-                                  [prone "1.5.2"]
-                                  [figwheel-sidecar "0.5.16"]
-                                  [nrepl "0.4.4"]
-                                  [cider/piggieback "0.3.8"]
-                                  [pjstadig/humane-test-output "0.8.3"]
+                   :dependencies [[cider/piggieback "0.4.2"]
+                                  [binaryage/devtools "0.9.11"]
+                                  [ring/ring-mock "0.4.0"]
+                                  [ring/ring-devel "1.8.0"]
+                                  [prone "2019-07-08"]
+                                  [figwheel-sidecar "0.5.19"]
+                                  [nrepl "0.6.0"]
+                                  [cider/piggieback "0.4.2"]
+                                  [pjstadig/humane-test-output "0.10.0"]
                                   
  ]
 
